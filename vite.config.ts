b336f8bc58139a475b7fcac391414bd5200c9ffc
siyichen98 <siyichen98@gmail.com>
2025-12-10 17:50:46 -0500

    final deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,18 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',               // 项目根目录
-  publicDir: 'public',     // 如果没有也没关系
+  root: '.',
+  publicDir: 'public',
+  base: '/theorbitweshare/',   // ← ← ← 关键！必须加！
+
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'), // ⭐告诉 Vite 入口是 index.html
+      input: 'index.html',
     },
   },
+
   resolve: {
     alias: {
       '@': path.resolve(process.cwd(), './'),
